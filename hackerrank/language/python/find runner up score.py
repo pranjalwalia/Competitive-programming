cd624e81370287634c68fdd53ad501382e5007c0
Hanzo hasashi <54119123+hanzohasashi33@users.noreply.github.com>
2019-10-15 21:13:27 +0530

    Update find runner up score.py

--- hackerrank/language/python/find runner up score.py
+++ hackerrank/language/python/find runner up score.py
@@ -1,7 +1,7 @@
-n = int(input())
-l = list(map(int,input().strip().split()))[:n]
-large = max(l)
-for i in l:
+n = int(input())                                   #initialising number of elements in list
+l = list(map(int,input().strip().split()))[:n]     
+large = max(l)                                     #initialising large as an element
+for i in l:                                         #for loop to check max
   if(large == max(l)):
       l.remove(max(l))
  print(max(l))
