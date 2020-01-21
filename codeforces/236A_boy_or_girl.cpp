a3324526483132e4432b81886be488c6b3ba94aa
Pranjal Walia <pranjalwalia77@gmail.com>
2020-01-21 12:04:48 +0530

    Create 236A_boy_or_girl.cpp

---
+++ codeforces/236A_boy_or_girl.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    char s[100];
+    cin >> s;
+    int count =0;
+    int l = strlen(s);
+    sort(s,s+l);
+    for(int i=0 ; i<l-1 ; i++){
+        if(s[i] != s[i+1]){
+            count++;
+        }
+    }
+    if(count%2!=0){
+        cout << "CHAT WITH HER!" << endl;
+    }
+    else{
+        cout << "IGNORE HIM!" << endl;
+    }
+
+}
