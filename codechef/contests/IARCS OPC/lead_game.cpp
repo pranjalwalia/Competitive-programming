17e33127683f4334bd08ab1dc2a8c6ebcf0b581f
Pranjal Walia <pranjalwalia77@gmail.com>
2020-01-27 20:45:19 +0530

    Create lead_game.cpp

---
+++ codechef/contests/IARCS OPC/lead_game.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    int a = 0, b = 0, lead = 0, win = 1;
+    
+    for(int i = 0; i < n; ++i){
+        int x, y;
+        cin >> x >> y;
+        a += x;
+        b += y;
+        int l = abs(a - b);
+        if(l > lead){
+            if(a>b){
+                win = 1;
+            }
+            else{
+                win = 2;
+            }
+            lead = l;
+        }
+    }
+    cout << win << " " << lead << endl;
+    
+    return 0;
+}
