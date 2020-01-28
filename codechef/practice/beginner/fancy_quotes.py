2a9d1ddb9a1b8e79b05cc335228eac7b7bf6dd21
Pranjal Walia <pranjalwalia77@gmail.com>
2020-01-28 15:01:55 +0530

    Create fancy_quotes.py

---
+++ codechef/practice/beginner/fancy_quotes.py
@@ -0,0 +1,10 @@
+t = int(input())
+for i in range(t):
+    s = str(input())
+    w = s.split()
+    s1 = 'not'
+
+    if(s1 in w):
+        print('Real Fancy')
+    else:
+        print("regularly fancy")
