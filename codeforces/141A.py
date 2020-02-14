baee09e23c2abe3f3bcd818802d1d535bb3796dc
Pranjal Walia <pranjalwalia77@gmail.com>
2020-02-14 14:56:14 +0530

    Create 141A.py

---
+++ codeforces/141A.py
@@ -0,0 +1,16 @@
+s1 = str(input())
+s2 = str(input())
+s3 = str(input())
+l1 = []
+l2 = []
+s4 = s1+s2
+for i in s4:
+    l1.append(i)
+l1.sort()
+for i in s3:
+    l2.append(i)
+l2.sort()
+if(l1==l2):
+    print('YES')
+else:
+    print('NO')    
