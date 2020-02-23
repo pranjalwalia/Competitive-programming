60f01a84cfff4345000772c3d0d79cbf61dee6b0
Pranjal Walia <pranjalwalia77@gmail.com>
2020-02-23 20:19:10 +0530

    Create A.cpp

---
+++ codeforces/contests/620_div2/A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        long long int x,y,a,b;
+        cin >> x >> y >> a >> b;
+        if((y-x)%(a+b)==0){
+            cout << (y-x)/(a+b) << endl;
+        }
+        else{
+            cout << -1 << endl;
+        }
+    }
+}
