b8810ee69cb03ff12c85b8c632b1cf7707ad15e6
pranjal <pranjalwalia77@gmail.com>
2020-05-12 00:06:49 +0530

    mst_kruskals_bruuuhh

---
+++ implementations/graphs/mst/Kruskals.cpp
@@ -0,0 +1,116 @@
+/*	Author --> @Pranjal Walia   IIIT Bangalore  */
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ff              				first
+#define ss              				second
+#define int             				long long
+#define pb              				push_back
+#define mp              				make_pair
+#define pii             				pair<int,int>
+#define vi              				vector<int>
+#define mii             				map<int,int>
+#define pqb             				priority_queue<int>
+#define pqs             				priority_queue<int,vi,greater<int> >
+#define setbits(x)      				__builtin_popcountll(x)
+#define zrobits(x)      				__builtin_ctzll(x)
+#define mod             				1000000007
+#define inf             				1e18
+#define mem(a,x,n)					memset(a,x,sizeof(a))		//initialise only 0 ans -1 with memset
+#define ps(x,y)         				fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  				type *arr=new type[n];
+#define w(x)            				int x; cin>>x; while(x--)
+#define sortv(v)        				sort(v.begin() , v.end())
+#define rsortv(v)       				sort(v.rbegin() , v.rend())
+#define pw(b,p)         				pow(b,p) + 0.1
+#define __          	   				ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define rep(i,a,b)	   					for(int i=a;i<b;i++)
+#define repb(i,a,b) 	   				for(int i=a;i>=b;i--)
+#define endl			   				"\n"    
+
+#define tr(x) cout<<x<<endl;
+#define tr2(x,y) cout<<x<<" "<<y<<endl;
+#define tr3(x,y,z) cout<<x<<" "<<y<<" "<<z<<endl;
+#define tr4(w,x,y,z) cout<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
+#define tr5(v,w,x,y,z) cout<<v<<" "<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
+#define tr6(u,v,w,x,y,z) cout<<u<<" "<<v<<" "<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
+
+#define sd(x) cin >> x;
+#define sd2(x,y) cin >> x >> y;
+#define sd3(x,y,z) cin >> x >> y >> z;
+#define sd4(w,x,y,z) cin >> w >> x >> y >> z;
+
+int mypow(int a, int b){	//(logn) --> faster than recursive --> binary expo
+    int res = 1;            // call int x = mypow(2,5);
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a)%mod;
+        a = (a * a)%mod;
+        b >>= 1;
+    }
+}
+
+void file(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt" , "r" , stdin);
+    freopen("output.txt" , "w" , stdout);
+    #endif
+}
+
+const int N = 1e5+5;
+int p[N];
+int rnk[N];
+
+void init(int n){
+    rep(i ,1 , n+1){
+        p[i] = i;
+        rnk[i]=1;
+    }
+}
+
+int find(int x){
+    if(p[x]==x)
+        return x;
+    return p[x] = find(p[x]);
+}
+
+void merge(int x, int y){
+    x=find(x);
+    y=find(y);
+
+    if(x!=y){
+        if(rnk[x]<rnk[y])
+            swap(x,y);
+        rnk[x]+=rnk[y];
+        p[y]=x;
+    }
+}
+
+int32_t main(){
+    __;
+    // file();
+    int n,e; sd2(n,e);
+    vector<pair<int,pii>> g;
+
+    rep(i,0 ,e){
+        int u,v,w; sd3(u,v,w);
+        g.pb({w , {u,v}});
+    }
+
+    // Kruskals 
+    int w=0;
+    sort(g.begin() , g.end());
+    init(n);    
+    for(auto itr : g){
+        int u_edge = itr.ss.ff;
+        int v_edge = itr.ss.ss;
+        
+        if(find(u_edge) != find(v_edge)){
+            w+=itr.ff;
+            merge(u_edge,  v_edge);
+        }
+    }
+
+    tr(w);
+    return 0;
+}
