5f15140d01122689513f23282256a4bd6e22e13b
Pranjal Walia <pranjalwalia77@gmail.com>
2020-06-13 19:21:03 +0530

    Update TDKPRIME.cpp

--- spoj/TDKPRIME.cpp
+++ spoj/TDKPRIME.cpp
@@ -1,23 +1,11 @@
-/*	Author
-@ Pranjal Walia
-IIIT Bangalore
-*/
 #include<bits/stdc++.h>
 using namespace std;
-
-void fastscan(int &x){
-bool neg = false;register int c;
-x = 0;c = getchar();
-if(c=='-'){neg = true;c = getchar();}
-for(; (c>47 && c<58); c=getchar()){x = x *10 + c - 48;}
-if(neg){x *= -1;}}
-
 #define ff              first
 #define ss              second
-// #define int             long long
+#define int             long long
 #define pb              push_back
 #define mp              make_pair
-#define pii             pair<int,int>
+#define pii              pair<int,int>
 #define vi              vector<int>
 #define mii             map<int,int>
 #define pqb             priority_queue<int>
@@ -29,21 +17,39 @@ if(neg){x *= -1;}}
 #define ps(x,y)         fixed<<setprecision(y)<<x
 #define mk(arr,n,type)  type *arr=new type[n];
 #define w(x)            int x; cin>>x; while(x--)
-#define sortv(v)        sort(v.begin() , v.end())
-#define rsortv(v)       sort(v.rbegin() , v.rend())
 #define pw(b,p)         pow(b,p) + 0.1
-#define __          	   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define rep(i,a,b)	   for(int i=a;i<b;i++)
-#define repb(i,a,b) 	   for(int i=a;i>=b;i--)
-#define endl			   "\n"    
+#define __              ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define rep(i,a,b) 		for(int i=a;i<b;i++)
+#define repb(i,a,b) 	for(int i=a;i>=b;i--)
+#define endl			"\n"
+#define m(a)		    memset(a,0,sizeof(a))
+#define all(x)                          begin(x), end(x)
+
+void fastscan(int &x){
+bool neg = false;register int c;
+x = 0;c = getchar();
+if(c=='-'){neg = true;c = getchar();}
+for(; (c>47 && c<58); c=getchar()){x = x *10 + c - 48;}
+if(neg){x *= -1;}}
 
 void file(){
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt" , "r" , stdin);
-    freopen("output.txt" , "w" , stdout);
-    #endif
+	#ifndef ONLINE_JUDGE
+	freopen("in.txt" , "r" , stdin);
+	freopen("out.txt" , "w" , stdout);
+	#endif
 }
 
+int bs(int a[] , int s , int e, int x){
+	if(s<=e){
+		int m = (s+e)/2;
+		if(a[m]==x)
+			return m;
+		else if(a[m] > x)
+			return bs(a,s,m-1,x);
+		else return bs(a,m+1,e,x);
+	}
+	return -1;
+}
 
 #define tr(x) cout<<x<<endl;
 #define tr2(x,y) cout<<x<<" "<<y<<endl;
@@ -52,28 +58,78 @@ void file(){
 #define tr5(v,w,x,y,z) cout<<v<<" "<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
 #define tr6(u,v,w,x,y,z) cout<<u<<" "<<v<<" "<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
 
-bool a[87000008];
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define deb3(x,y,z) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<","<<#z<<"="<<z<<endl
+
+#define sd(x) cin >> x;
+#define sd2(x,y) cin >> x >> y;
+#define sd3(x,y,z) cin >> x >> y >> z;
+#define sd4(w,x,y,z) cin >> w >> x >> y >> z;
+
+bool is_pal(string s) {return equal(all(s), s.rbegin());}
+
+int mypow(int a, int b){        //(logn) fast exponentiation
+    int res = 1;            // call int x = mypow(2,5);
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a)%mod;
+        a = (a * a)%mod;
+        b >>= 1;
+    }
+    return (res%mod);
+}
+
 vi primes;
+bool a[90000001];
 
 void sieve(){
-    int maxN = 87000009;
-    a[0] = a[1] = true;
-
-    for(int i=2 ; i<=sqrt(maxN) ; i++){
-        if(a[i]==false){
-            primes.pb(i);
-            for(int j=i*i ; j<=maxN ; j++)
-                a[j]=true;
-        }
-    }
+	int maxN = 90000000;
+	a[0] = a[1] = 1;
+	for(int i=2 ; i*i<=maxN ; i++){
+		if(!a[i]){
+			for(int j = i*i ; j<=maxN ; j+=i){
+				a[j] = 1;
+			}
+		}
+	}
+	rep(i , 2 , maxN){
+		if(!a[i]){
+			primes.pb(i);
+		}
+	}
 }
 
-int32_t main(){
-    __;	
-    int q; cin >> q;
-    while(q--){
-        int k; cin >> k;
-        cout << primes[k-1] << endl;
-    }
-    return 0;
+
+int32_t main(){	
+	__;
+	file();
+	sieve();
+	w(t){
+		int x; sd(x);
+		cout << primes[x-1] << endl;
+	}
+	return 0;
 }
+
+/*
+2
+29
+541
+7919
+104729
+1299709
+15485863
+
+7
+1
+10
+100
+1000
+10000
+100000
+1000000
+
+*/
+
+
