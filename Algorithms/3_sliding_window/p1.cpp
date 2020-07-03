ace151ef07b1d320afd6d5079d966ad3d1a1f9e6
Pranjal Walia <pranjalwalia77@gmail.com>
2020-07-03 23:45:38 +0530

    Create p1.cpp

---
+++ Algorithms/3_sliding_window/p1.cpp
@@ -0,0 +1,133 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii              pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define pw(b,p)         pow(b,p) + 0.1
+#define __              ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define rep(i,a,b) 		for(int i=a;i<b;i++)
+#define repb(i,a,b) 	for(int i=a;i>=b;i--)
+#define endl			"\n"
+#define m(a)		    memset(a,0,sizeof(a))
+#define all(x)                          begin(x), end(x)
+
+void fastscan(int &x){
+bool neg = false;register int c;
+x = 0;c = getchar();
+if(c=='-'){neg = true;c = getchar();}
+for(; (c>47 && c<58); c=getchar()){x = x *10 + c - 48;}
+if(neg){x *= -1;}}
+
+void file(){
+	#ifndef ONLINE_JUDGE
+	freopen("in.txt" , "r" , stdin);
+	freopen("out.txt" , "w" , stdout);
+	#endif
+}
+
+int bs(int a[] , int s , int e, int x){
+	if(s<=e){
+		int m = (s+e)/2;
+		if(a[m]==x)
+			return m;
+		else if(a[m] > x)
+			return bs(a,s,m-1,x);
+		else return bs(a,m+1,e,x);
+	}
+	return -1;
+}
+
+#define tr(x) cout<<x<<endl;
+#define tr2(x,y) cout<<x<<" "<<y<<endl;
+#define tr3(x,y,z) cout<<x<<" "<<y<<" "<<z<<endl;
+#define tr4(w,x,y,z) cout<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
+#define tr5(v,w,x,y,z) cout<<v<<" "<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
+#define tr6(u,v,w,x,y,z) cout<<u<<" "<<v<<" "<<w<<" "<<x<<" "<<y<<" "<<z<<endl;
+
+#define deb(x) cout<<#x<<"="<<x<<endl
+#define deb2(x,y) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<endl
+#define deb3(x,y,z) cout<<#x<<"="<<x<<","<<#y<<"="<<y<<","<<#z<<"="<<z<<endl
+
+#define sd(x) cin >> x;
+#define sd2(x,y) cin >> x >> y;
+#define sd3(x,y,z) cin >> x >> y >> z;
+#define sd4(w,x,y,z) cin >> w >> x >> y >> z;
+
+bool is_pal(string s) {return equal(all(s), s.rbegin());}
+
+int mypow(int a, int b){        //(logn) fast exponentiation
+    int res = 1;            // call int x = mypow(2,5);
+    while (b > 0) {
+        if (b & 1)
+            res = (res * a)%mod;
+        a = (a * a)%mod;
+        b >>= 1;
+    }
+    return (res%mod);
+}
+/*
+5
+8 6
+1 2 4 1 2 4 1 2
+5 3
+3 2 3 2 1
+10 4
+4 3 4 3 2 3 2 1 0 1
+15 7
+3 7 4 8 2 3 4 5 21 2 3 4 2 1 3
+7 5
+1 2 3 4 5 6 1
+
+3 2
+2 2
+2 1
+3 1
+2 3
+
+*/
+
+
+int32_t main(){	
+	__;
+	file();
+	w(t){
+		int n,k;sd2(n,k);
+		int a[n]; rep(i, 0 ,n) sd(a[i]);
+		int p[n]; p[0] = 0; p[n-1]=0;
+		for(int i=1; i<n-1 ; i++){
+			if(a[i] > a[i-1] && a[i] > a[i+1]) p[i]=1;
+			else p[i]=0;
+		}
+		int window = 0;
+		for(int i=1 ; i<k-1 ; i++) window+=p[i];
+		int ans=window;
+		int pos=0;
+		for(int i=k-1 ; i<n ; i++){
+			window+=(p[i]-p[i-(k-2)]);
+			if(window > ans){
+				ans = window;
+				pos=i-(k-2);
+			}
+		}
+		cout << ans+1 << " " << pos+1 << endl;
+	}
+	return 0;
+}
+
+
+
+
