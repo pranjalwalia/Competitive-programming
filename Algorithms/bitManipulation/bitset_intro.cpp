0a74cec2d91669c2efd124743038d0835c2d5027
Pranjal Walia <pranjalwalia77@gmail.com>
2020-07-09 22:41:35 +0530

    Update bitset_intro.cpp

--- Algorithms/bitManipulation/bitset_intro.cpp
+++ Algorithms/bitManipulation/bitset_intro.cpp
@@ -1,3 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii              pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+#define pw(b,p)         pow(b,p) + 0.1
+#define __              ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define rep(i,a,b) 		for(int i=a;i<b;i++)
+#define repb(i,a,b) 	for(int i=a;i>=b;i--)
+#define endl			"\n"
+#define m(a)		    memset(a,0,sizeof(a))
+#define all(x)                          begin(x), end(x)
+
+void fastscan(int &x){
+bool neg = false;register int c;
+x = 0;c = getchar();
+if(c=='-'){neg = true;c = getchar();}
+for(; (c>47 && c<58); c=getchar()){x = x *10 + c - 48;}
+if(neg){x *= -1;}}
+
+void file(){
+	#ifndef ONLINE_JUDGE
+	freopen("in.txt" , "r" , stdin);
+	freopen("out.txt" , "w" , stdout);
+	#endif
+}
+
+int bs(int a[] , int s , int e, int x){
+	if(s<=e){
+		int m = (s+e)/2;
+		if(a[m]==x)
+			return m;
+		else if(a[m] > x)
+			return bs(a,s,m-1,x);
+		else return bs(a,m+1,e,x);
+	}
+	return -1;
 }
 
 #define tr(x) cout<<x<<endl;
@@ -29,30 +80,28 @@ int mypow(int a, int b){        //(logn) fast exponentiation
     return (res%mod);
 }
 
-int32_t main(){
-        __;
-        file();
-        int n; sd(n);
-        bitset<32> b(n);
+int32_t main(){	
+	__;
+	file();
+	int n; sd(n);
+	bitset<32> b(n);
 
-        cout << b.to_string() << endl;
-        cout << b.to_ulong() << endl;   //unsigned long
-        cout << b.to_ullong() << endl;  //unsigned long long
-
-
-
-        return 0;
+	cout << b.to_string() << endl;
+	cout << b.to_ulong() << endl;	//unsigned long
+	cout << b.to_ullong() << endl;	//unsigned long long 
+	
+	
+	
+	return 0;
 }
 
 /*
-        Intro to bitset..1e9 => 32 bit..
-        bitset<size> name(n);   //indexing from lsb..
+	Intro to bitset..1e9 => 32 bit..
+	bitset<size> name(n);	//indexing from lsb..
 
-        1. bitset can directly store n in binary form..
-        2. following conversions are possible: decimal => binary, binary=>string, binary=>decimal.
-        3. modifications in bitset also possible...
+	1. bitset can directly store n in binary form..
+	2. following conversions are possible: decimal => binary, binary=>string, binary=>decimal.
+	3. modifications in bitset also possible...
 */
 
-
-bitset_intro.cpp [dos] (03:47 07/07/2020)                                                                                                                                                             86,14-21 Bot
 
