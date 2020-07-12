af3e9d7d570be591f73d0e6b7e3d90759eb7bfb9
Pranjal Walia <pranjalwalia77@gmail.com>
2020-07-12 18:57:54 +0530

    Added setup shell script

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#MY SHELL SCRIPT TO SET UP THE INITIAL ENVIRONMENT..Currently under development..
+#MY SHELL SCRIPT TO SET UP THE INITIAL ENVIRONMENT..Currently under development...
 echo "Setting Things Up..."
 CUR_DATE=`date +%d`
 mkdir $CUR_DATE && touch $CUR_DATE/main.cpp && touch $CUR_DATE/in.txt && touch $CUR_DATE/out.txt && touch $CUR_DATE/p1.cpp && touch $CUR_DATE/p2.cpp && touch $CUR_DATE/p3.cpp
