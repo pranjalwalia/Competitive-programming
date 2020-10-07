25f2e3850de5722f239d30fdc4f753572f509c08
MC01-dev <69892043+MC01-dev@users.noreply.github.com>
2020-10-07 17:19:38 +0530

    code refactor

--- Hackerrank/problem solving/Picking_Numbers.cpp
+++ Hackerrank/problem solving/Picking_Numbers.cpp
@@ -1,54 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ff              first
-#define ss              second
-#define int             long long
-#define pb              push_back
-#define mp              make_pair
-#define pii             pair<int,int>
-#define vi              vector<int>
-#define mii             map<int,int>
-#define pqb             priority_queue<int>
-#define pqs             priority_queue<int,vi,greater<int> >
-#define setbits(x)      __builtin_popcountll(x)
-#define zrobits(x)      __builtin_ctzll(x)
-#define mod             1000000007
-#define inf             1e18
-#define ps(x,y)         fixed<<setprecision(y)<<x
-#define mk(arr,n,type)  type *arr=new type[n];
-#define w(x)            int x; cin>>x; while(x--)
-#define pw(b,p)         pow(b,p) + 0.1
-
-void file(){
-
-    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt" , "r" , stdin);
-    freopen("output.txt" , "w" , stdout);
-    #endif
-}
-
 int32_t main(){    
-        int n;
-        cin >>n;
-        mk(a,n,int);
+        int n; cin >>n;
+        int a[n];
         for(int i=0 ; i<n ; i++)
             cin >> a[i];
-
         sort(a,a+n);
         int ans=0;
-
-        for(int i=0 ; i<n; i++){
+        for(int i=0 ; i<n; ++i){
             int count=0;
-            for(int j=i+1 ; j<n ;j++){
-                if( abs(a[i]-a[j])<=1 ){
-                    count++;
-                }
+            for(int j=i+1 ; j<n ;++j){
+                if( abs(a[i]-a[j])<=1 )
+                    count++;             
             }
             ans = max(ans , count);
         }
-
-        cout << ans+1 << endl;
-
+        cout << ++ans << endl;
+    return 0;
 }
